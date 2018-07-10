da755ab382670bea8419ab20a65dfff9ff1ae672
Chris Perko <chris@chrisperko.net>
2018-07-10 19:39:28 -0400

    Can now move and edit polygons

--- polygon/script.js
+++ polygon/script.js
@@ -8,6 +8,14 @@ require([
     "dojo/domReady!"
 ], (MapView, Map, SketchViewModel, Graphic, GraphicsLayer, GeometryEngine) => {
 
+    const validSymbol = createSymbol(
+        [0, 170, 255, 0.8], "solid", 2, [255, 255, 255]
+    );
+
+    const invalidSymbol = createSymbol(
+        [255, 0, 0], "diagonal-cross", 4, [255, 0, 0]
+    );
+
     // GraphicsLayer to hold graphics created via sketch view model
     const graphicsLayer = new GraphicsLayer();
 
@@ -24,7 +32,7 @@ require([
     });
 
     let sketch = new SketchViewModel({
-        view: view,
+        view,
         layer: graphicsLayer
     });
 
@@ -33,31 +41,69 @@ require([
     });
 
     sketch.on('create-complete', event => {
-        let symbol = sketch.graphic.symbol;
-        symbol.color = [0,0,0,0.3];
+        let graphic = new Graphic({
+            geometry: event.geometry,
+            symbol: validSymbol,
+        });
 
         graphicsLayer.graphics.items.forEach(existingGraphic => {
             let disjoint = GeometryEngine.disjoint(event.geometry, existingGraphic.geometry);
             if (!disjoint) {
-                symbol.color = [255,0,0, 0.5];
+                graphic.symbol = invalidSymbol;
             }
         });
 
-        graphicsLayer.add(new Graphic({
-            geometry: event.geometry,
-            symbol
-        }));
+        graphicsLayer.add(graphic);
     });
 
     view.on('click', (event) => {
+        let isUpdate = false;
         view.hitTest(event).then(response => {
             // console.log(response.results);
-            if(response.results) {
-                console.log(response.results[0].graphic);
+            if (response.results) {
                 sketch.update(response.results[0].graphic);
+                isUpdate = true;
             }
         });
-        sketch.create('polygon');
+
+        if(!isUpdate) sketch.create('polygon');
     });
+
+    function createSymbol(color, style, width, outlineColor) {
+        return {
+            type: "simple-fill",
+            style: style,
+            color: color,
+            outline: {
+                color: outlineColor,
+                width: width
+            }
+        }
+    }
+
+
+    sketch.on("update-init", updateInit);
+    sketch.on("move-start", checkGraphicUpdate);
+    sketch.on("move", checkGraphicUpdate);
+    sketch.on("move-complete", checkGraphicUpdate);
+    sketch.on("scale-start", checkGraphicUpdate);
+    sketch.on("scale", checkGraphicUpdate);
+    sketch.on("scale-complete", checkGraphicUpdate);
+    sketch.on("rotate-start", checkGraphicUpdate);
+    sketch.on("rotate", checkGraphicUpdate);
+    sketch.on("rotate-complete", checkGraphicUpdate);
+    sketch.on("reshape-start", checkGraphicUpdate);
+    sketch.on("reshape", checkGraphicUpdate);
+    sketch.on("reshape-complete", checkGraphicUpdate);
+
+    let editObject = null;
+
+    function updateInit(event) {
+        editObject = event.graphic;
+    }
+
+    function checkGraphicUpdate(event) {
+        editObject.geometry = event.geometry;
+    }
 
 });
