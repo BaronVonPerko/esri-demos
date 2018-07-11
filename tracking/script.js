64380e8f510d7f2f8380267969bded4f82908c2a
Chris Perko <chris@chrisperko.net>
2018-07-10 20:06:24 -0400

    Print points to the map

--- tracking/script.js
+++ tracking/script.js
@@ -2,11 +2,18 @@ require([
     "esri/widgets/Track",
     "esri/views/MapView",
     "esri/Map",
+    "esri/Graphic",
+    "esri/geometry/Point",
+    "esri/widgets/Sketch/SketchViewModel",
+    "esri/layers/GraphicsLayer",
     "dojo/domReady!"
-], (Track, MapView, Map) => {
+], (Track, MapView, Map, Graphic, Point, SketchViewModel, GraphicsLayer) => {
+
+    const graphicsLayer = new GraphicsLayer();
 
     let map = new Map({
-        basemap: "topo"
+        basemap: "satellite",
+        layers: [graphicsLayer],
     });
 
     let view = new MapView({
@@ -14,10 +21,15 @@ require([
         container: "viewDiv"
     });
 
+    let sketch = new SketchViewModel({
+        view,
+        layer: graphicsLayer
+    });
+
     // Create an instance of the Track widget
     // and add it to the view's UI
     let track = new Track({
-        view
+        view,
     });
     view.ui.add(track, "top-left");
 
@@ -25,5 +37,26 @@ require([
     // once the view becomes ready
     view.when(() => {
         track.start();
+    });
+
+    track.on('track', event => {
+        let point = new Point({
+            longitude: event.position.coords.longitude,
+            latitude: event.position.coords.latitude,
+        });
+
+        let pointGraphic = new Graphic({
+            geometry: point,
+            symbol: {
+                type: "simple-marker", // autocasts as new SimpleMarkerSymbol()
+                color: [226, 119, 40],
+                outline: { // autocasts as new SimpleLineSymbol()
+                    color: [255, 255, 255],
+                    width: 2
+                }
+            },
+        });
+
+        view.graphics.add(pointGraphic);
     });
 });
