ec838d49f8dd697fb39b4c160d08dea83dbf0b41
Chris Perko <christopher.j.perko@windstream.com>
2018-07-11 19:44:53 -0400

    es6 the things in the single file

--- layers-es6/script.js
+++ layers-es6/script.js
@@ -6,23 +6,21 @@ require([
         "dojo/on",
         "dojo/domReady!"
     ],
-    function(
-        Map, SceneView, TileLayer, dom, on
-    ) {
+    (Map, SceneView, TileLayer, dom, on) => {
 
         /*****************************************************************
          * Create two TileLayer instances. One pointing to a
          * cached map service depicting U.S. male population and the other
          * pointing to a layer of roads and highways.
          *****************************************************************/
-        var transportationLayer = new TileLayer({
+        let transportationLayer = new TileLayer({
             url: "https://server.arcgisonline.com/ArcGIS/rest/services/Reference/World_Transportation/MapServer",
             // This property can be used to uniquely identify the layer
             id: "streets",
             visible: false
         });
 
-        var housingLayer = new TileLayer({
+        let housingLayer = new TileLayer({
             url: "https://tiles.arcgis.com/tiles/nGt4QxSblgDfeJn9/arcgis/rest/services/New_York_Housing_Density/MapServer",
             id: "ny-housing",
             opacity: 0.9
@@ -31,26 +29,21 @@ require([
         /*****************************************************************
          * Layers may be added to the map in the map's constructor
          *****************************************************************/
-        var map = new Map({
+        let map = new Map({
             basemap: "oceans",
-            layers: [housingLayer]
+            layers: [housingLayer, transportationLayer]
         });
 
-        /*****************************************************************
-         * Or they may be added to the map using map.add()
-         *****************************************************************/
-        map.add(transportationLayer);
-
-        var view = new SceneView({
+        let view = new SceneView({
             container: "viewDiv",
-            map: map
+            map
         });
 
         /*****************************************************************
          * The map handles the layers' data while the view and layer views
          * take care of renderering the layers
          *****************************************************************/
-        view.on("layerview-create", function(event) {
+        view.on("layerview-create", (event) => {
             if (event.layer.id === "ny-housing") {
                 // Explore the properties of the population layer's layer view here
                 console.log("LayerView for male population created!", event.layerView);
@@ -67,13 +60,13 @@ require([
          * properties may be accessed. Once the population layer has loaded,
          * the view will animate to it's initial extent.
          *****************************************************************/
-        view.when(function() {
-            housingLayer.when(function() {
+        view.when(() => {
+            housingLayer.when(() => {
                 view.goTo(housingLayer.fullExtent);
             });
         });
 
-        var streetsLayerToggle = dom.byId("streetsLayer");
+        let streetsLayerToggle = dom.byId("streetsLayer");
 
         /*****************************************************************
          * The visible property on the layer can be used to toggle the
@@ -81,7 +74,7 @@ require([
          * the layer is still part of the map, which means you can access
          * its properties and perform analysis even though it isn't visible.
          *******************************************************************/
-        on(streetsLayerToggle, "change", function() {
+        on(streetsLayerToggle, "change", () => {
             transportationLayer.visible = streetsLayerToggle.checked;
         });
     });
