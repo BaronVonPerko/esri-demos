a96f22e9f3b7e828c2451eccbce8a9a1c0ce41f2
Chris Perko <chris@chrisperko.net>
2018-07-16 16:36:37 -0400

    Fixed bugs

--- pwa/script.js
+++ pwa/script.js
@@ -3,34 +3,34 @@ require([
     "esri/views/MapView",
     "esri/Map",
     "dojo/domReady!"
-], function(
+], function (
     Track, MapView, Map
 ) {
 
-    var map = new Map({
-        basemap: "topo"
+    let map = new Map({
+        basemap: "streets"
     });
 
-    var view = new MapView({
-        map: map,
+    let view = new MapView({
+        map,
         container: "viewDiv"
     });
 
     // Create an instance of the Track widget
     // and add it to the view's UI
-    var track = new Track({
-        view: view,
+    let track = new Track({
+        view,
         geolocationOptions: {
             maximumAge: 0,
-            timeout: 0,
+            timeout: 500,
             enableHighAccuracy: true
-        }
+        },
     });
     view.ui.add(track, "top-left");
 
     // The sample will start tracking your location
     // once the view becomes ready
-    view.when(function() {
+    view.when(function () {
         track.start();
     });
 });
